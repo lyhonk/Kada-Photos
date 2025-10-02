167ad1dc4ee3658f3ddbd6f33f856ab776e11cfe
Jimmy Chen <shouhukongling@163.com>
2025-10-02 08:48:56 +0800

    ``` feat(main): 实现多模式切换与相册功能 - 新增日历模式和相册模式，支持通过按键切换 - 重构图片下载逻辑，增强健壮性与错误处理 - 改进 GPIO 唤醒检测逻辑，区分不同唤醒源行为 - 更新配网提示逻辑，指定说明图片路径 - 完善深度睡眠配置与 RGB LED 状态反馈 ```

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,8 @@ HSPI_SCLK -> 12
 HSPI_COPI -> 13
 
 按键接法
-左 (切换模式)     -> 1
-中 (重置无线密码) -> 2
+左 (切换模式)     -> 2
+中 (重置系统)     -> 3
 右 (立即刷新图片) -> 11
 */
 
@@ -24,6 +24,7 @@ HSPI_COPI -> 13
 #include <SPI.h>
 #include "epd7in3f.h"
 #include "LittleFS.h"
+#include "driver/rtc_io.h"
 
 #define useLittleFS
 #include <BGWiFiConfig.h>
@@ -38,19 +39,23 @@ HSPI_COPI -> 13
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 
+#include <ArduinoJson.h>
+
+#include <Preferences.h> // 使用Preferences库来保存数据
+Preferences preferences;
+
 // NTP客户端设置
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "ntp.aliyun.com", 8 * 3600, 60000); // UTC+8时区，每60秒更新一次
 
-#define BUTTON_PIN_BITMASK(GPIO) (1ULL << GPIO)  // 2 ^ GPIO_NUMBER in hex
-#define WAKEUP_GPIO_1              GPIO_NUM_1
-#define WAKEUP_GPIO_2              GPIO_NUM_2
-#define WAKEUP_GPIO_10             GPIO_NUM_10
-#define WAKEUP_GPIO_11             GPIO_NUM_11
+#define BUTTON_PIN_BITMASK(GPIO) (1ULL << GPIO) // 2 ^ GPIO_NUMBER in hex
+#define WAKEUP_GPIO_2 GPIO_NUM_2
+#define WAKEUP_GPIO_3 GPIO_NUM_3
+#define WAKEUP_GPIO_10 GPIO_NUM_10
+#define WAKEUP_GPIO_11 GPIO_NUM_11
 
 // Define bitmask for multiple GPIOs
-uint64_t bitmask = BUTTON_PIN_BITMASK(WAKEUP_GPIO_1) | BUTTON_PIN_BITMASK(WAKEUP_GPIO_2) | BUTTON_PIN_BITMASK(WAKEUP_GPIO_10) | BUTTON_PIN_BITMASK(WAKEUP_GPIO_11);
-
+uint64_t bitmask = BUTTON_PIN_BITMASK(WAKEUP_GPIO_2) | BUTTON_PIN_BITMASK(WAKEUP_GPIO_3) | BUTTON_PIN_BITMASK(WAKEUP_GPIO_10) | BUTTON_PIN_BITMASK(WAKEUP_GPIO_11);
 
 RTC_DS3231 rtc;
 
@@ -82,11 +87,7 @@ void setRTCTimeFromNTP()
   timeClient.end();
 }
 
-// 图片 URL 和本地保存路径
-const char *imageUrl = "http://statics.biogeeker.com/demo.bmp";
-const char *filePath = "/display.bmp";
-const char *infoFilePath = "/demo.bmp";
-bool downloadImage()
+bool downloadImage(String imageUrl, String filePath)
 {
   if (!LittleFS.begin(true))
   { // 初始化 LittleFS 用于临时存储下载的图片
@@ -101,11 +102,14 @@ bool downloadImage()
   Serial.println(imageUrl);
 
   http.begin(client, imageUrl);
+  http.setTimeout(5000);        // 设置整体超时时间为5秒
+  http.setConnectTimeout(3000); // 设置连接超时为3秒
+
   int httpCode = http.GET();
 
   if (httpCode != HTTP_CODE_OK)
   {
-    Serial.printf("HTTP request failed with code %d\n", httpCode);
+    Serial.printf("HTTP 请求失败，状态码：%d\n", httpCode);
     http.end();
     return false;
   }
@@ -122,16 +126,31 @@ bool downloadImage()
   uint8_t buffer[1024];
   size_t len;
 
+  bool success = true;
+
   while ((len = response->readBytes(buffer, sizeof(buffer))) > 0)
   {
-    file.write(buffer, len);
+    if (file.write(buffer, len) != len)
+    {
+      Serial.println("写入文件时出现错误");
+      success = false;
+      break;
+    }
   }
 
   file.close();
   http.end();
 
-  Serial.println("图片已下载并保存！");
-  return true;
+  if (success)
+  {
+    Serial.println("图片已成功下载并保存！");
+  }
+  else
+  {
+    Serial.println("图片下载失败");
+  }
+
+  return success;
 }
 
 Epd epd;
@@ -145,7 +164,6 @@ uint16_t read16(fs::File &f)
   ((uint8_t *)&result)[1] = f.read(); // MSB
   return result;
 }
-
 uint32_t read32(fs::File &f)
 {
   uint32_t result;
@@ -156,12 +174,17 @@ uint32_t read32(fs::File &f)
   return result;
 }
 
-bool drawBmp(const char *filename)
+bool drawBmp(String filename)
 {
   fs::File bmpFS;
 
   // Open requested file on LittleFS
   bmpFS = LittleFS.open(filename, "r");
+  if (!bmpFS)
+  {
+    Serial.println("无法打开图片文件！");
+    return false;
+  }
   Serial.println("读取图片中。。。。");
 
   uint32_t seekOffset, headerSize, paletteSize = 0;
@@ -170,8 +193,8 @@ bool drawBmp(const char *filename)
   uint16_t bitDepth;
 
   uint16_t magic = read16(bmpFS);
-  if (magic != ('B' | ('M' << 8)))
-  { // File not found or not a BMP
+  if (magic != 0x4D42) // 'BM'
+  {
     Serial.println(F("BMP not found!"));
     bmpFS.close();
     return false;
@@ -185,6 +208,7 @@ bool drawBmp(const char *filename)
   uint32_t h = read32(bmpFS); // height
   read16(bmpFS);              // color planes (must be 1)
   bitDepth = read16(bmpFS);
+
   if (read32(bmpFS) != 0 || (bitDepth != 24 && bitDepth != 1 && bitDepth != 4 && bitDepth != 8))
   {
     Serial.println(F("BMP format not recognized."));
@@ -304,39 +328,38 @@ bool drawBmp(const char *filename)
   return true;
 }
 
-void displayImage()
+void displayImage(String imageUrl, String filePath)
 {
   if (epd.Init() != 0)
   {
     Serial.print("墨水屏启动失败");
     return;
   }
-  if (!downloadImage())
+
+  bool success = downloadImage(imageUrl, filePath);
+
+  if (!success)
   {
     Serial.println("图片下载失败");
-  }
-  else
-  {
-    Serial.print("图片正在生成\r\n ");
-    drawBmp(filePath);
-    Serial.print("图片显示完成\r\n ");
     epd.Sleep();
+    return;
   }
+
+  Serial.print("图片加载中。。。。");
+  drawBmp(filePath);
+  Serial.print("图片显示完成");
+  epd.Sleep();
 }
 
-void displayWiFIinfo()
+void displayWiFIinfo(String filePath)
 {
   if (epd.Init() != 0)
   {
     Serial.print("墨水屏启动失败");
     return;
   }
-  if (!LittleFS.exists("/bgwificonfig/wifiset.txt"))
-  {
-    Serial.println("未发现网络配置文件！");
-  }
-  drawBmp(infoFilePath); 
-  Serial.print("Done\r\n ");
+  drawBmp(filePath);
+  Serial.print("说明图片显示完成");
   epd.Sleep();
 }
 
@@ -363,16 +386,333 @@ void restpasswd()
   Serial.println("配网信息清除成功");
 }
 
-void setup()
+void print_GPIO_wake_up()
 {
+  int GPIO_reason = esp_sleep_get_ext1_wakeup_status();
+  Serial.print("GPIO that triggered the wake up: GPIO ");
+  Serial.println((log(GPIO_reason)) / log(2), 0);
+}
 
-  // put your setup code here, to run once:
-  pinMode(48, OUTPUT);
-  digitalWrite(48, LOW);
+void print_wakeup_reason()
+{
+  esp_sleep_wakeup_cause_t wakeup_reason;
 
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch (wakeup_reason)
+  {
+  case ESP_SLEEP_WAKEUP_EXT0:
+    Serial.println("Wakeup caused by external signal using RTC_IO");
+    break;
+  case ESP_SLEEP_WAKEUP_EXT1:
+    Serial.println("Wakeup caused by external signal using RTC_CNTL");
+    print_GPIO_wake_up();
+    break;
+  case ESP_SLEEP_WAKEUP_TIMER:
+    Serial.println("Wakeup caused by timer");
+    break;
+  case ESP_SLEEP_WAKEUP_TOUCHPAD:
+    Serial.println("Wakeup caused by touchpad");
+    break;
+  case ESP_SLEEP_WAKEUP_ULP:
+    Serial.println("Wakeup caused by ULP program");
+    break;
+  default:
+    Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeup_reason);
+    break;
+  }
+}
+
+String getUUID(String url)
+{
+  HTTPClient http;
+  WiFiClient client;
+  String result;
+
+  http.begin(client, url);
+  http.setTimeout(5000);
+  int httpResponseCode = http.GET();
+  if (httpResponseCode == HTTP_CODE_OK)
+  {
+    Serial.println("HTTP GET request succeeded");
+    String payload = http.getString();
+    JsonDocument doc;
+    DeserializationError error = deserializeJson(doc, payload);
+    if (error)
+    {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+    }
+    else
+    {
+      result = doc["uuid"].as<String>();
+    }
+  }
+  else
+  {
+    Serial.printf("HTTP GET failed, error: %s\n", http.errorToString(httpResponseCode).c_str());
+    result = String();
+  }
+  http.end();
+  return result;
+}
+
+int getPhotoCount(String url)
+{
+  HTTPClient http;
+  WiFiClient client;
+  int result = 0;
+
+  http.begin(client, url);
+  http.setTimeout(5000);
+  int httpResponseCode = http.GET();
+  if (httpResponseCode == HTTP_CODE_OK)
+  {
+    Serial.println("HTTP GET request succeeded");
+    String payload = http.getString();
+    JsonDocument doc;
+    DeserializationError error = deserializeJson(doc, payload);
+    if (error)
+    {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      http.end();
+      return 0;
+    }
+    result = doc["data"].size();
+  }
+  else
+  {
+    Serial.printf("HTTP GET failed, error: %s\n", http.errorToString(httpResponseCode).c_str());
+  }
+  http.end();
+  return result;
+}
+
+String getPhotoUrl(String url, int index)
+{
+  HTTPClient http;
+  WiFiClient client;
+  String result;
+
+  http.begin(client, url);
+  http.setTimeout(5000);
+  int httpResponseCode = http.GET();
+  if (httpResponseCode == HTTP_CODE_OK)
+  {
+    Serial.println("HTTP GET request succeeded");
+    String payload = http.getString();
+    JsonDocument doc;
+    DeserializationError error = deserializeJson(doc, payload);
+    if (error)
+    {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      http.end();
+    }
+    result = doc["data"][index].as<String>();
+  }
+  else
+  {
+    Serial.printf("HTTP GET failed, error: %s\n", http.errorToString(httpResponseCode).c_str());
+    result = String();
+  }
+  http.end();
+  return result;
+}
+
+void blinktest()
+{
+  for (int i = 0; i < 10; i++)
+  {
+    digitalWrite(RGB_BUILTIN, HIGH);
+    delay(300);
+    digitalWrite(RGB_BUILTIN, LOW);
+    delay(300);
+  }
+}
+
+void calendar_modle()
+{
+  Serial.println("当前模式为：日历模式");
+  // 同步网络时间并打印
+  setRTCTimeFromNTP();
+  printDetailedRTCInfo();
+
+  // 清除所有闹钟，确保上次的闹钟不会影响本次
+  rtc.clearAlarm(1);
+  rtc.clearAlarm(2);
+  // 设置一个固定的闹钟时间：凌晨 00:02:00
+  // TODO：联网后获取触发时间，并设置闹钟
+  DateTime midnight(2000, 1, 1, 0, 2, 0); // 年月日是占位符，不影响闹钟触发
+
+  // 设置闹钟1，匹配时、分、秒后触发
+  // 这样可以确保闹钟在每天的 00:00:00 准时触发
+  rtc.setAlarm1(midnight, DS3231_A1_Hour);
+
+  // 打印闹钟时间，便于确认
+  Serial.printf("闹钟已设置，将在每天 %02d:%02d:%02d 唤醒\n",
+                midnight.hour(), midnight.minute(), midnight.second());
+
+  // 图片 URL 和本地保存路径
+  String imageUrl = "http://statics.biogeeker.com/demo.bmp";
+  String filePath = "/display.bmp";
+
+  displayImage(imageUrl, filePath);
+}
+
+void photo_modle()
+{
+  Serial.println("当前模式为：相册模式");
+  Serial.println("当前为相册模式");
+  // 同步网络时间并打印
+  setRTCTimeFromNTP();
+  printDetailedRTCInfo();
+
+  // 清除所有闹钟，确保上次的闹钟不会影响本次
+  rtc.clearAlarm(1);
+  rtc.clearAlarm(2);
+
+  // 获取当前时间
+  DateTime now = rtc.now();
+
+  // 设置下一个整点时间（分钟和秒数都为0）
+  DateTime nextHour(now.year(), now.month(), now.day(), now.hour() + 1, 0, 0);
+
+  // 设置闹钟1，每小时整点触发
+  rtc.setAlarm1(nextHour, DS3231_A1_Hour);
+
+  // 打印闹钟时间，便于确认
+  Serial.printf("闹钟已设置，将在每小时 %02d:%02d 整点唤醒\n",
+                nextHour.hour(), nextHour.minute());
+  
+  String photolistUrl = "http://statics.biogeeker.com/kada/photolist.json";
+  String uuid = getUUID(photolistUrl);
+  Serial.println("相册队列uuid为:");
+  Serial.println(uuid);
+  String currentuuid = preferences.getString("uuid", "inituuid");
+
+  int photoIndex = preferences.getUInt("index", 0);
+
+  if (currentuuid != uuid)
+  {
+    Serial.println("数据更新了");
+    preferences.putString("uuid", uuid);
+    preferences.putUInt("index", 0);
+
+    // 图片 URL 和本地保存路径
+    String photoimageUrl = getPhotoUrl(photolistUrl, photoIndex);
+    String photpdisplay = "/photpdisplay.bmp";
+    displayImage(photoimageUrl, photpdisplay);
+
+    photoIndex = photoIndex + 1;
+    preferences.putUInt("index", photoIndex);
+  }
+  else
+  {
+    Serial.println("数据没有更新");
+    photoIndex = preferences.getUInt("index", 0);
+    Serial.println("当前图片索引为:");
+    Serial.println(photoIndex);
+    if (photoIndex >= getPhotoCount(photolistUrl))
+    {
+      photoIndex = 0;
+      preferences.putUInt("index", photoIndex);
+    }
+    // 图片 URL 和本地保存路径
+    String photoimageUrl = getPhotoUrl(photolistUrl, 0);
+    String photpdisplay = "/photpdisplay.bmp";
+    displayImage(photoimageUrl, photpdisplay);
+  }
+
+}
+void check_EXT_info()
+{
+  uint64_t GPIO_reason = esp_sleep_get_ext1_wakeup_status();
+  int modle = preferences.getUInt("modle", 0);
+
+  // 检查是否有 GPIO 被触发
+  if (GPIO_reason == 0)
+  {
+    Serial.println("No GPIO triggered the wake up.");
+    if (modle == 0)
+    {
+      calendar_modle();
+    }
+    else
+    {
+      photo_modle();
+    }
+    return;
+  }
+
+  // 检查是否只有一个 GPIO 被触发（即是否为 2 的幂）
+  if ((GPIO_reason & (GPIO_reason - 1)) != 0)
+  {
+    Serial.println("Multiple GPIOs triggered the wake up.");
+    return;
+  }
+
+  // 使用位运算获取最低置位的 GPIO 编号
+  int GPIO_number = __builtin_ctzll(GPIO_reason);
+
+  Serial.print("GPIO that triggered the wake up: GPIO ");
+  Serial.println(GPIO_number);
+
+  switch (GPIO_number)
+  {
+  case 2:
+    Serial.println("切换模式");
+    rgbLedWrite(RGB_BUILTIN, RGB_BRIGHTNESS, 0, 0); // Red
+
+    if (modle == 0)
+    {
+      photo_modle();
+      bool success = preferences.putUInt("modle", 1);
+      if (!success)
+      {
+        Serial.println("状态保存失败");
+      }
+    }
+    else
+    {
+      calendar_modle();
+      bool success = preferences.putUInt("modle", 0);
+      if (!success)
+      {
+        Serial.println("状态保存失败");
+      }
+    }
+    break;
+  case 3:
+    // 重置相册无线密码
+    Serial.println("重置相册无线密码");
+    rgbLedWrite(RGB_BUILTIN, 0, RGB_BRIGHTNESS, 0); // Green
+    delay(2000);
+    restpasswd();
+    ESP.restart();
+    break;
+  case 10:
+    // 接收DS3231定时重启信号
+    Serial.println("定时重启");
+    break;
+  case 11:
+    // 强制刷新
+    digitalWrite(RGB_BUILTIN, HIGH); // Turn the RGB LED white
+    delay(2000);
+    Serial.println("强制刷新");
+    ESP.restart();
+  default:
+    Serial.println("其他原因重启");
+    break;
+  }
+
+  delay(5000); // 可考虑替换为非阻塞方式，如 millis()
+}
+void init_system()
+{
   Serial.begin(115200);
   delay(1000);
-
   if (!rtc.begin())
   {
     Serial.println("未找到 DS3231 RTC 模块!");
@@ -395,12 +735,25 @@ void setup()
     Serial.println("LittleFS 已启动");
     delay(1000);
   }
+}
+void setup()
+{
+  // put your setup code here, to run once:
+  pinMode(48, OUTPUT);
+  digitalWrite(48, LOW);
+
+  init_system();
+
+  preferences.begin("kada-photos", false);
+  int modle = preferences.getUInt("modle", 0);
 
   if (!LittleFS.exists("/bgwificonfig/wifiset.txt"))
   {
     Serial.println("######");
     Serial.println("展示配网方法");
-    displayWiFIinfo();
+    // 配网说明图片
+    String infoFilePath = "/demo.bmp";
+    displayWiFIinfo(infoFilePath);
     Serial.println("######");
   }
 
@@ -428,46 +781,29 @@ void setup()
   wifipw.setCompatibleMode(true);
 
   wifipw.begin();
-
   if (wifipw.OK())
   {
-    // 同步网络时间并打印
-    setRTCTimeFromNTP();
-    printDetailedRTCInfo();
+    check_EXT_info();
+  }
+  // Print the wakeup reason for ESP32
+  print_wakeup_reason();
 
-    // 清除所有闹钟，确保上次的闹钟不会影响本次
-    rtc.clearAlarm(1);
-    rtc.clearAlarm(2);
-    // 设置一个固定的闹钟时间：凌晨 00:02:00
-    // TODO：联网后获取触发时间，并设置闹钟
-    DateTime midnight(2000, 1, 1, 0, 2, 0); // 年月日是占位符，不影响闹钟触发
+  // Use ext1 as a wake-up source
+  esp_sleep_enable_ext1_wakeup_io(bitmask, ESP_EXT1_WAKEUP_ANY_LOW);
+  // enable pull-down resistors and disable pull-up resistors
+  rtc_gpio_pulldown_dis(WAKEUP_GPIO_2);
+  rtc_gpio_pullup_en(WAKEUP_GPIO_2);
+  rtc_gpio_pulldown_dis(WAKEUP_GPIO_3);
+  rtc_gpio_pullup_en(WAKEUP_GPIO_3);
+  rtc_gpio_pulldown_dis(WAKEUP_GPIO_10);
+  rtc_gpio_pullup_en(WAKEUP_GPIO_10);
+  rtc_gpio_pulldown_dis(WAKEUP_GPIO_11);
+  rtc_gpio_pullup_en(WAKEUP_GPIO_11);
 
-    // 设置闹钟1，匹配时、分、秒后触发
-    // 这样可以确保闹钟在每天的 00:00:00 准时触发
-    rtc.setAlarm1(midnight, DS3231_A1_Hour);
-
-    // 打印闹钟时间，便于确认
-    Serial.printf("闹钟已设置，将在每天 %02d:%02d:%02d 唤醒\n",
-                  midnight.hour(), midnight.minute(), midnight.second());
-
-    // 显示图片
-    displayImage();
-
-    // 配置外部唤醒源
-    esp_sleep_enable_ext1_wakeup_io(bitmask, ESP_EXT1_WAKEUP_ANY_LOW);
-    
-    gpio_pulldown_dis(WAKEUP_GPIO_1);
-    gpio_pullup_en(WAKEUP_GPIO_1);
-    gpio_pulldown_dis(WAKEUP_GPIO_2);
-    gpio_pullup_en(WAKEUP_GPIO_2);
-    gpio_pulldown_dis(WAKEUP_GPIO_11);
-    gpio_pullup_en(WAKEUP_GPIO_11);
-
-    Serial.println("进入深度睡眠...");
-    Serial.flush();
-    // 进入深度睡眠模式
-    esp_deep_sleep_start();
-  };
+  // Go to sleep now
+  Serial.println("Going to sleep now");
+  digitalWrite(RGB_BUILTIN, LOW);
+  esp_deep_sleep_start();
 }
 
 void loop()
